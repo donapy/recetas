551cc3a3ba3c85cf6d01a5323553e542eb75a2a1
Donato Garcia <616dona@gmail.com>
2023-03-12 15:02:15 -0300

    added multer and fix some imports

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
-const connectDB = require("./config/db");
+const connectDB = require("./configs/db");
 const port = process.env.PORT || 5000;
 
 connectDB();
