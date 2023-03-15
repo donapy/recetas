4db49872ccd337d3629d6a839526fe0d86f0e5d0
divinatroche <divina0210@gmail.com>
2023-03-14 21:58:53 -0300

    added loading component, change font in the index

--- client/src/Components/Loading.jsx
+++ client/src/Components/Loading.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import ReactLoading from "react-loading";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return <div>
+    <ReactLoading type="bubbles" color="#5C1B39"
+        height={100} width={50} />
+  </div>;
 };
 
 export default Loading;
