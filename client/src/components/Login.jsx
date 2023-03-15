2504a39e5f32ff17e86333739ac46fe3061af7a8
Diana Nakagawa <drcnakagawa@gmail.com>
2023-03-15 00:29:30 -0300

    changed the login, register and navbar components

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Label, TextInput, Button, Toast } from "flowbite-react";
-import logo from "../assets/logo.png";
+import React, {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {Label, TextInput, Button, Toast, Card} from "flowbite-react";
+import full from "../assets/full.png";
 import axios from "axios";
-import { HiExclamation } from "react-icons/hi";
+import {HiExclamation} from "react-icons/hi";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -14,7 +14,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const inputs = { email, password };
+    const inputs = {email, password};
     // axios
     //   .post("http://localhost:8000/api/login", user, {
     //     withCredentials: true,
@@ -24,10 +24,10 @@ const Login = () => {
 
     const validationErrors = {};
     if (!email) {
-      validationErrors.email = { message: "Email is required" };
+      validationErrors.email = {message: "Email is required"};
     }
     if (!password) {
-      validationErrors.password = { message: "Password is required" };
+      validationErrors.password = {message: "Password is required"};
     }
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
@@ -63,68 +63,73 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex justify-evenly items-center h-screen mx-auto py-auto">
-        <div>
-          <img src={logo} alt="logo" height="300px" />
-          <h1 className="font-bold text-4xl"> Cauldron Cuisine</h1>
+      <div className="flex items-center h-screen mx-auto py-auto">
+        <div className="w-1/2">
+          <div className="w-2/3 mx-auto ">
+            <img src={full} alt="logo" />
+          </div>
         </div>
-        <form onSubmit={handleSubmit} className="register-container">
-          <h1 className="text-2xl font-bold text-center py-4">Log In</h1>
-          <div className="text-1xl pb-2">
-            <div className="mb-1 block">
-              <Label htmlFor="email1" value="Your email" />
-            </div>
-            <TextInput
-              id="email1"
-              type="email"
-              placeholder="name@mail.com"
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {errors.email ? (
-              <div className="space-x-4 divide-x divide-gray-200 dark:divide-gray-700">
-                <Toast>
-                  <HiExclamation className="h-5 w-5" />
-                  <div className="pl-4 text-sm font-normal">
-                    {errors.email.message}
+        <div className="w-1/2 mx-auto">
+          <Card className="max-w-sm text-center">
+            <form onSubmit={handleSubmit}>
+              <h1 className="text-2xl font-bold text-center my-4">Log In</h1>
+              <div className="text-1xl pb-2">
+                <div className="mb-1 block">
+                  <Label htmlFor="email1" value="Your email" />
+                </div>
+                <TextInput
+                  id="email1"
+                  type="email"
+                  placeholder="name@mail.com"
+                  required={true}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {errors.email ? (
+                  <div className="space-x-4 divide-x divide-gray-200 dark:divide-gray-700">
+                    <Toast>
+                      <HiExclamation className="h-5 w-5" />
+                      <div className="pl-4 text-sm font-normal">
+                        {errors.email.message}
+                      </div>
+                    </Toast>
                   </div>
-                </Toast>
+                ) : (
+                  ""
+                )}
               </div>
-            ) : (
-              ""
-            )}
-          </div>
-          <div className="text-1xl pb-2 pt-2">
-            <div className="mb-1 block">
-              <Label htmlFor="password1" value="Your password" />
-            </div>
-            <TextInput
-              id="password1"
-              type="password"
-              required={true}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {errors.password ? (
-              <div className="space-x-4 divide-x divide-gray-200 dark:divide-gray-700">
-                <Toast>
-                  <HiExclamation className="h-5 w-5" />
-                  <div className="pl-4 text-sm font-normal">
-                    {errors.password.message}
+              <div className="text-1xl pb-2 pt-2">
+                <div className="mb-1 block">
+                  <Label htmlFor="password1" value="Your password" />
+                </div>
+                <TextInput
+                  id="password1"
+                  type="password"
+                  required={true}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {errors.password ? (
+                  <div className="space-x-4 divide-x divide-gray-200 dark:divide-gray-700">
+                    <Toast>
+                      <HiExclamation className="h-5 w-5" />
+                      <div className="pl-4 text-sm font-normal">
+                        {errors.password.message}
+                      </div>
+                    </Toast>
                   </div>
-                </Toast>
+                ) : (
+                  ""
+                )}
               </div>
-            ) : (
-              ""
-            )}
-          </div>
-          <Button
-            color="success"
-            className="mx-auto w-3/4 font-semibold pt-4"
-            type="submit"
-          >
-            Log In
-          </Button>
-        </form>
+              <Button
+                color="success"
+                className="mx-auto w-3/4 font-semibold my-4"
+                type="submit"
+              >
+                Log In
+              </Button>
+            </form>
+          </Card>
+        </div>
       </div>
     </>
   );
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import { Label, TextInput, Button } from "flowbite-react";
-import logo from "../assets/logo.png";
+import {useNavigate} from "react-router-dom";
+import {Label, TextInput, Button, Card} from "flowbite-react";
+import full from "../assets/full.png";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -15,7 +15,7 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const newUser = { name, email, password, confirmPassword };
+    const newUser = {name, email, password, confirmPassword};
     await axios
       .post(
         "http://localhost:8000/api/user/newUsuario",
@@ -35,79 +35,84 @@ const Register = () => {
 
   return (
     <>
-      <div className="flex justify-evenly items-center h-screen mx-auto py-auto">
-        <div>
-          <img src={logo} alt="logo" height="300px" />
-          <h1 className="font-bold text-4xl"> Cauldron Cuisine</h1>
+      <div className="flex items-center h-screen mx-auto py-auto">
+        <div className="w-1/2">
+          <div className="w-2/3 mx-auto ">
+            <img src={full} alt="logo" />
+          </div>
         </div>
-        <form onSubmit={handleSubmit} className="register-container">
-          <h1 className="text-2xl font-bold text-center py-4">Register</h1>
-          {/* Name */}
-          <div className="text-1xl pb-2">
-            <div className="mb-1 block">
-              <Label htmlFor="name" value="Your name" />
-            </div>
-            <TextInput
-              id="name"
-              type="text"
-              placeholder="Enter your name here"
-              required={true}
-              onChange={(e) => setName(e.target.value)}
-            />
-            {errors.name ? <p>{errors.name.message}</p> : ""}
-          </div>
-          {/* email */}
-          <div className="text-1xl pb-2">
-            <div className="mb-1 block">
-              <Label htmlFor="email1" value="Your email" />
-            </div>
-            <TextInput
-              id="email1"
-              type="email"
-              placeholder="your@mail.com"
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {errors.email ? <p>{errors.email.message}</p> : ""}
-          </div>
-          {/* password */}
-          <div className="text-1xl pb-2 pt-2">
-            <div className="mb-1 block">
-              <Label htmlFor="password1" value="Your password" />
-            </div>
-            <TextInput
-              id="password1"
-              type="password"
-              required={true}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {errors.password ? <p>{errors.password.message}</p> : ""}
-          </div>
-          {/* confirm password */}
-          <div className="text-1xl pb-2 pt-2">
-            <div className="mb-1 block">
-              <Label htmlFor="password1" value="Confirm password" />
-            </div>
-            <TextInput
-              id="password1"
-              type="password"
-              required={true}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            {errors.confirmPassword ? (
-              <p>{errors.confirmPassword.message}</p>
-            ) : (
-              ""
-            )}
-          </div>
-          <Button
-            color="success"
-            className="mx-auto w-3/4 font-semibold pt-4"
-            type="submit"
-          >
-            Register
-          </Button>
-        </form>
+        <div className="w-1/2 mx-auto">
+          <Card className="max-w-sm">
+            <form onSubmit={handleSubmit}>
+              <h1 className="text-2xl font-bold text-center py-4">Register</h1>
+              {/* Name */}
+              <div className="text-1xl pb-2">
+                <div className="mb-1 block">
+                  <Label htmlFor="name" value="Your name" />
+                </div>
+                <TextInput
+                  id="name"
+                  type="text"
+                  placeholder="Enter your name here"
+                  required={true}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                {errors.name ? <p>{errors.name.message}</p> : ""}
+              </div>
+              {/* email */}
+              <div className="text-1xl pb-2">
+                <div className="mb-1 block">
+                  <Label htmlFor="email1" value="Your email" />
+                </div>
+                <TextInput
+                  id="email1"
+                  type="email"
+                  placeholder="your@mail.com"
+                  required={true}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {errors.email ? <p>{errors.email.message}</p> : ""}
+              </div>
+              {/* password */}
+              <div className="text-1xl pb-2 pt-2">
+                <div className="mb-1 block">
+                  <Label htmlFor="password1" value="Your password" />
+                </div>
+                <TextInput
+                  id="password1"
+                  type="password"
+                  required={true}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {errors.password ? <p>{errors.password.message}</p> : ""}
+              </div>
+              {/* confirm password */}
+              <div className="text-1xl pb-2 pt-2">
+                <div className="mb-1 block">
+                  <Label htmlFor="password1" value="Confirm password" />
+                </div>
+                <TextInput
+                  id="password1"
+                  type="password"
+                  required={true}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                {errors.confirmPassword ? (
+                  <p>{errors.confirmPassword.message}</p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <Button
+                color="success"
+                className="mx-auto w-3/4 font-semibold my-4"
+                type="submit"
+              >
+                Register
+              </Button>
+            </form>
+          </Card>
+        </div>
       </div>
     </>
   );
